78021b31f392ec1c7242cd9b8faf438ca75bebe8
Subilan <suyuanliang1995@gmail.com>
2025-09-19 20:27:39 +0800

    refactor: reimplementation of ChoiceList, avoiding *key* problem

--- src/components/form-elements/Choice.vue
+++ src/components/form-elements/Choice.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
-    import { ChoiceList } from '@ownego/polaris-vue';
+    import { BlockStack, Checkbox, InlineStack, RadioButton, TextField } from '@ownego/polaris-vue';
     import FormElement from './FormElement.vue';
-    import { computed, h, ref, resolveComponent, type PropType } from 'vue';
+    import { reactive, ref, watch, type PropType } from 'vue';
     import type { ChoiceResult } from '@/models/form/elements/Choice';
     import type { FormEl } from '@/models/form/Form';
 
@@ -12,45 +12,41 @@
         }
     });
 
-    const selected = ref<string[]>([]);
     const otherValue = ref('');
 
-    const otherChoice = h(resolveComponent('TextField'), {
-        labelHidden: true,
-        modelValue: otherValue.value,
-        autoComplete: 'off',
-        'onUpdate:modelValue': (value: string) => {
-            otherValue.value = value;
-        }
-    });
+    // 多选，适用于 Checkbox
+    const isChecked = reactive<Record<string, boolean>>({});
 
-    const finalChoices = computed(() => {
-        // let base: ChoiceOption[] = [
-        //     { label: 'Hidden', value: 'hidden', helpText: 'example help text' },
-        //     { label: 'Optional', value: 'optional' },
-        //     { label: 'Required', value: 'required' }
-        // ];
+    // 单选，适用于 RadioButton
+    const singleChoice = ref('');
 
-        let base = [...props.self.config.choices];
-
-        if (props.self.config.hasOther) {
-            base.push({
-                label: props.self.config.otherLabel || 'Other',
-                value: 'other',
-                renderChildren: selected.value.includes('other') && otherChoice
-            })
-        }
-
-        return base;
-    });
+    watch(singleChoice, v => {
+        clearChecked();
+        isChecked[v] = true;
+    })
 
     function get(): ChoiceResult {
-        return {
-            selection: selected.value,
+        let result: ChoiceResult = {
+            selection: Object.keys(isChecked).filter(k => isChecked[k]),
             otherValue: otherValue.value,
-            valid: selected.value.length > 0
+            valid: false,
         };
+
+        result.valid = result.selection.length > 0;
+        return result;
     }
+
+    /**
+     * 重置选择状态
+     */
+    function clearChecked() {
+        // @ts-ignore
+        Object.keys(isChecked).forEach(k => isChecked[k] = false);
+    }
+
+    watch(() => props.self.config.isMultiple, v => {
+        clearChecked();
+    })
 
     defineExpose({
         get
@@ -59,6 +55,19 @@
 
 <template>
     <FormElement>
-        <ChoiceList :choices="finalChoices" v-model="selected" :allow-multiple="self.config.isMultiple" />
+        <BlockStack>
+            <template v-if="self.config.isMultiple">
+                <Checkbox :label="x.label" :help-text="x.helpText" v-for="x in self.config.choices"
+                    v-model="isChecked[x.id]" />
+            </template>
+            <template v-else>
+                <RadioButton :name="`radio-for-${self.identifier}`" :label="x.label" :help-text="x.helpText"
+                    :value="x.id" v-for="x in self.config.choices" v-model="singleChoice" />
+            </template>
+            <InlineStack gap="400" v-if="self.config.hasOther">
+                <Checkbox label="其它" v-model="isChecked['other']" />
+                <TextField size="slim" v-model="otherValue" auto-complete="off" v-if="isChecked['other']" :placeholder="self.config.otherLabel || '输入你的答案'" />
+            </InlineStack>
+        </BlockStack>
     </FormElement>
 </template>
