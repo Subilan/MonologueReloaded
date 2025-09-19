abaf76a7764d72fb3ec404daac3f5aaa4cd0e8b9
Subilan <suyuanliang1995@gmail.com>
2025-09-19 20:49:36 +0800

    chore: element config base

---
+++ src/components/form-elements/configuration/Config.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+    import type { FormElement } from '@/models/form/Form';
+    import { FormLayout, InlineStack, TextField } from '@ownego/polaris-vue';
+
+    const config = defineModel<FormElement>({ required: true });
+</script>
+
+<template>
+    <FormLayout>
+        <TextField auto-complete="off" read-only v-model="config.identifier" label="标识符" />
+        <TextField auto-complete="off" v-model="config.title" label="标题" placeholder="输入标题" help-text="该表单元素的主要内容" />
+        <TextField auto-complete="off" v-model="config.description" :multiline="5" label="介绍文本" placeholder="输入文本">
+            <template #helpText>
+                <InlineStack gap="100">
+                    <img class="support-markdown-logo" src="/images/language-markdown-outline.svg"/>
+                    <span>该表单元素的具体描述信息，显示在标题的下方</span>
+                </InlineStack>
+            </template>
+        </TextField>
+        <slot />
+    </FormLayout>
+</template>
+
+<style lang="scss" scoped>
+.support-markdown-logo {
+    pointer-events: none;
+    height: 20px;
+    opacity: .5;
+}
+</style>
---
+++ src/components/form-elements/configuration/GenericConfig.vue
@@ -0,0 +1,23 @@
+<template>
+    <component :is="targetComponent" v-model="model" />
+</template>
+
+<script lang="ts" setup>
+    import type { FormElement, FormElementType } from '@/models/form/Form';
+    import ChoiceConfig from './ChoiceConfig.vue';
+    import { computed } from 'vue';
+
+    const model = defineModel<FormElement>({ required: true });
+
+    const map: Record<FormElementType, any> = {
+        'choice': ChoiceConfig,
+        select: undefined,
+        text_input: undefined,
+        paragraph_input: undefined,
+        slider: undefined,
+        rating: undefined,
+        unknown: undefined
+    }
+
+    const targetComponent = computed(() => map[model.value.type])
+</script>
