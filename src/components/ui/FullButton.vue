48ab42435331d99fd45071ec57c94c24e0369f8e
Subilan <suyuanliang1995@gmail.com>
2025-09-20 14:40:26 +0800

    ui: FullButton, NumberTextfield assistive component

---
+++ src/components/ui/FullButton.vue
@@ -0,0 +1,9 @@
+<template>
+    <Button variant="primary" style="width: 100%;">
+        <slot />
+    </Button>
+</template>
+
+<script lang="ts" setup>
+    import { Button } from '@ownego/polaris-vue';
+</script>
---
+++ src/components/ui/NumberTextField.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+    import { TextField } from '@ownego/polaris-vue';
+    import { ref, watch } from 'vue';
+
+    const model = defineModel<number>({ default: 0 });
+
+    const props = defineProps({
+        min: {
+            type: Number
+        },
+        max: {
+            type: Number
+        },
+        step: {
+            type: Number
+        },
+        label: {
+            type: String
+        },
+        helpText: {
+            type: String
+        },
+        suffix: {
+            type: String
+        }
+    })
+
+    const val = ref(model.value.toString());
+    watch(val, v => model.value = Number(v));
+</script>
+
+<template>
+    <TextField :label="label" :suffix="suffix" :help-text="helpText" auto-complete="off" v-model="val" :min="min" :max="max" :step="step" type="number"/>
+</template>
