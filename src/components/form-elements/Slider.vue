93d514c061a249a12b0a19822d778295d749db35
Subilan <suyuanliang1995@gmail.com>
2025-09-27 13:01:09 +0800

    fix: explicit bindings, fix bug in dual/single slider switching

--- src/components/form-elements/Slider.vue
+++ src/components/form-elements/Slider.vue
@@ -1,32 +1,40 @@
 <script setup lang="ts">
-    import { RangeSlider } from '@ownego/polaris-vue';
-    import FormElement from './FormElement.vue';
-    import { ref, type PropType } from 'vue';
-    import type { FormEl, SimpleResult } from '@/models/form/Form';
+import { RangeSlider } from '@ownego/polaris-vue';
+import FormElement from './FormElement.vue';
+import { computed, ref, watch, type PropType } from 'vue';
+import type { FormEl, SimpleResult } from '@/models/form/Form';
 
-    const props = defineProps({
-        self: {
-            type: Object as PropType<FormEl<'slider'>>,
-            required: true
-        }
-    });
-
-    const value = ref(props.self.config.isRange ? [0, 0] as [number, number] : 0);
-
-    function get(): SimpleResult<number | number[]> {
-        return {
-            valid: true,
-            value: value.value
-        };
+const props = defineProps({
+    self: {
+        type: Object as PropType<FormEl<'slider'>>,
+        required: true
     }
+});
 
-    defineExpose({
-        get
-    });
+const minValue = computed(() => props.self.config.minValue || 0)
+
+const singleValue = ref(0);
+const rangeValue = ref<[number, number]>([minValue.value, minValue.value]);
+
+function get(): SimpleResult<number | number[]> {
+    return {
+        valid: true,
+        value: props.self.config.isRange ? rangeValue.value : singleValue.value
+    };
+}
+
+defineExpose({
+    get
+});
 </script>
 
 <template>
     <FormElement>
-        <RangeSlider :label="self.config.label" v-model="value" v-bind="self.config" />
+        <RangeSlider v-if="self.config.isRange" :label="self.config.label" v-model="rangeValue"
+            :min="self.config.minValue" :max="self.config.maxValue" :step="self.config.step"
+            :prefix="self.config.prefix" :suffix="self.config.suffix" :output="self.config.output" />
+        <RangeSlider v-else :label="self.config.label" v-model="singleValue" :min="self.config.minValue"
+            :max="self.config.maxValue" :step="self.config.step" :prefix="self.config.prefix"
+            :suffix="self.config.suffix" :output="self.config.output" />
     </FormElement>
 </template>
